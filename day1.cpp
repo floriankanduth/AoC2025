0300d70b0b379360f5f47db184ac10b961973c2d
floriankanduth <kanduthflorian@gmail.com>
2025-12-10 14:01:46 +0100

    day1.2 ugly brute force

--- day1.cpp
+++ day1.cpp
@@ -1,19 +1,36 @@
-// Source - https://stackoverflow.com/a
-// Posted by 111111, modified by community. See post 'Timeline' for change history
-// Retrieved 2025-12-09, License - CC BY-SA 3.0
+/**
+ * https://adventofcode.com/2025/day/1
+ */
 
 #include <cinttypes>
 #include <fstream>
 #include <iostream>
 #include <string>
 using namespace std;
+//#define DEBUG
+
+#ifdef DEBUG
+#define DEBUG_PRINT(...) printf(__VA_ARGS__)
+#else
+#define DEBUG_PRINT(...)
+#endif
 
 
+/**
+ *
+ * @param line line from puzzle input
+ * @return distance to turn the dial
+ */
 int get_distance(const string &line) {
     const string distance_str = line.substr(1, line.length() - 1);
     return stoi(distance_str);
 }
 
+/**
+ *
+ * @param line line from puzzle input
+ * @return direction factor as +-1
+ */
 int get_dir(const string &line) {
     return line[0] == 'L' ? -1 : 1;
 }
@@ -27,15 +44,25 @@ int main() {
 
 
     while (std::getline(file, line)) {
-        std::cout << line << std::endl;
-        position += get_dir(line) * get_distance(line);
-        position %= 100;
-        printf("New position: %d\n", position);
+        const int dir = get_dir(line);
+        const int dist = get_distance(line);
 
-        if (position == 0) {
-            count++;
+        DEBUG_PRINT("%s", line);
+
+        for (int i = 0; i < dist; i++) {
+            position += dir;
+            position %= 100;
+            position = position < 0 ? position + 100 : position;
+            if (position == 0) {
+                count++;
+                DEBUG_PRINT("count: %d\n", count);
+            }
         }
+
+        DEBUG_PRINT("New position: %d\n", position);
     }
-    printf("Count: %d\n", count);
+    printf("final count: %d\n", count);
+
+    file.close();
     return 0;
 }
