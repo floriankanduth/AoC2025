ed964ddcc525c76c158848abdc7bb8ba7e14528c
floriankanduth <kanduthflorian@gmail.com>
2025-12-12 17:18:33 +0100

    init day 3

--- src/day3.cpp
+++ src/day3.cpp
@@ -2,6 +2,25 @@
  * https://adventofcode.com/2025/day/3
  */
 
+#include "common.h"
+
+/**
+ * Find maximum joltage by switching on 2 batteries in each line.
+ * 987654321111111 -> 98
+ * ^^
+ * 811111111111119 -> 89
+ * ^             ^
+ * 234234234234278 -> 78
+ *              ^^
+ * 818181911112111 -> 92
+ *       ^    ^
+ *
+ * @return
+ */
+auto solve_part1() {
+
+}
 int main(int argc, char **argv) {
+    auto lines = readFile(3);
 
 }
